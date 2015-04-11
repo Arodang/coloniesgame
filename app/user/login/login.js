04de40881130350a78a98902885de4287156f9db
Dan Inglin <sfbattlefield2@gmail.com>
2015-04-11 05:42:10 -0700

    add angularfire-seed core files

---
+++ app/user/login/login.js
@@ -0,0 +1,11 @@
+var loginModule = angular.module('login', ['firebase']);
+
+loginModule
+	.controller('loginController', ['$scope', function($scope) {
+		
+	]})
+	
+	.factory('loginService', ['$firebaseAuth', function($firebaseAuth) {
+		
+	}]);
+	
