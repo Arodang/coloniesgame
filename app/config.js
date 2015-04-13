817b8dc5537e2ea2f3c853d550b9b261fd17aefc
Dan Inglin <sfbattlefield2@gmail.com>
2015-04-12 20:59:53 -0700

    Updated config.js to point to firebase app, included .travis.yml file for TravisCI builds.

--- app/config.js
+++ app/config.js
@@ -11,7 +11,7 @@ angular.module('myApp.config', [])
   .constant('loginRedirectPath', '/login')
 
   // your Firebase data URL goes here, no trailing slash
-  .constant('FBURL', 'https://blazing-inferno-2674.firebaseio.com')
+  .constant('FBURL', 'https://colonies-game.firebaseio.com')
 
   // double check that the app has been configured before running it and blowing up space and time
   .run(['FBURL', '$timeout', function(FBURL, $timeout) {
